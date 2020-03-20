2123a7cb764544fdbe1a748625b9abcde4755776
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2020-03-20 07:35:24 +0200

    Update deps

--- src/data/CmsClient.ts
+++ src/data/CmsClient.ts
@@ -1,17 +1,31 @@
-import { createClient } from 'contentful/dist/contentful.browser.min.js';
+import { createClient, Entry } from 'contentful/dist/contentful.browser.min.js';
 import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } from 'react-native-dotenv';
+import { CmsSchedule, CmsTask } from './CmsTypes';
+import { Schedule, Task } from './Schedule';
+
+const space =  CONTENTFUL_SPACE_ID ?? "space-id-missing"
 
 class CmsClient {
   private readonly client = createClient({
-    space: CONTENTFUL_SPACE_ID ?? "space-id-missing",
+    space,
     accessToken: CONTENTFUL_ACCESS_TOKEN ?? "access-token-missing"
   });
 
   getSchedule = async () => {
-    const data = await this.client.getEntries({ content_type: "schedule" });
-    console.log("Got data from contentful:", data)
+    const data = await this.client.getEntries<CmsSchedule>({ content_type: "schedule" });
+    console.log("Got data from contentful:", data.items.map(toSchedule))
   }
+}
 
+function toSchedule(data: Entry<CmsSchedule>) {
+  return { 
+    name: data.fields?.name ?? "Nimetön",
+    tasks: (data.fields?.tasks ?? []).map(toTask),
+  }
+}
+
+function toTask(data: Entry<CmsTask>) {
+  console.log("image", data.fields.image?.fields);
 }
 
 export const cmsClient = new CmsClient()
---
+++ src/data/CmsTypes.ts
@@ -0,0 +1,27 @@
+import {Entry} from 'contentful';
+import {ISOTime} from './Time';
+
+export interface CmsImage {
+  file: {
+    contentType: string;
+    details: {
+      size: number;
+      fileName: string;
+      url: string;
+    };
+  };
+  title: string;
+}
+
+export interface CmsTask {
+  name: string;
+  color: string;
+  startTime?: ISOTime;
+  endTime?: ISOTime;
+  image?: Entry<CmsImage>;
+}
+
+export interface CmsSchedule {
+  name: string;
+  tasks: Entry<CmsTask>[];
+}
